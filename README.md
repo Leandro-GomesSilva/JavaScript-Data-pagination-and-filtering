7d2c5b42460e3bcc8ca555e1cabf825758290dfb
Le-Gomes <69648505+Le-Gomes@users.noreply.github.com>
2020-09-02 23:41:47 +0200

    Functions showList and addPagination
    First try for the functions showList and addPagination. Still this moment, the showList seems to be working fine and the addPagination needs some improvement.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # techdegree-javascript-project-2
- My 2nd project on the Full Stack JavaScript Techdegree:
- Data pagination and filtering.
+ My 2nd project on the Full Stack JavaScript Techdegree: Data pagination and filtering.
--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
@@ -17,7 +15,51 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+   const itemsPerPage = 9;
+   
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   
+   const ul = document.querySelector('.student-list');
+   ul.innerHTML = '';
 
+   for (let i = 0; i < list.length; i += 1) {
+      
+      if ( i >= startIndex && i <= endIndex ) {
+         
+         function createElement (tagName, tagClassName) {
+            const tag = document.createElement(tagName);
+            if (tagClassName) {
+               tag.className = tagClassName;
+            }
+            return tag;
+         }
+
+         const li = createElement('li', 'student-item cf');
+         const divStudentDetails = createElement('div', 'student-details');
+         const img = createElement('img', 'avatar');
+         const h3 = createElement('h3', '');
+         const spanEmail = createElement('span', 'email');
+         const divJoinedDetails = createElement('div', 'joined-details');
+         const spanDate = createElement('span', 'date');
+
+         img.src = list[i].picture.large;
+         img.alt = 'Profile Picture';
+         h3.textContent = list[i].name.first + ' ' + list[i].name.last;
+         spanEmail.textContent = list[i].email;
+         spanDate.textContent = 'Joined ' + list[i].registered.date;
+
+         ul.appendChild(li);
+         li.appendChild(divStudentDetails);
+         divStudentDetails.appendChild(img);
+         divStudentDetails.appendChild(h3);
+         divStudentDetails.appendChild(spanEmail);
+         li.appendChild(divJoinedDetails);
+         divJoinedDetails.appendChild(spanDate);
+      }
+   }
+}
 
 
 /*
@@ -25,6 +67,40 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+   const itemsPerPage = 9;
+   const numberButtons = Math.ceil(list.length / itemsPerPage);
+
+   const ul = document.querySelector('.link-list');
+   ul.innerHTML = '';
+
+   for (let i = 1; i <= numberButtons; i += 1) {
+      const li = document.createElement('li');
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.textContent = i;
+      li.appendChild(button);
+      ul.appendChild(li);
+   }
+
+   const firstButton = ul.querySelector('li');
+   firstButton.className = 'active';
+   
+   ul.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+         targetButton = e.target;
+         let li = ul.children;
+         for (let i = 0; i < li.length; i += 1) {
+            li[i].firstElementChild.className = '';
+         }
+         e.target.className = 'active';
+         showPage(data, 3)
+      }
+   });
+
+}
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
